dd8b6d89890bccf5a13989d51648ccd4cdbec2bd
mahmoud-gargouri <150925003+mahmoud-gargouri@users.noreply.github.com>
2024-10-24 15:26:18 +0100

    Update supplier.js routes

--- routes/supplier.js
+++ routes/supplier.js
@@ -8,5 +8,7 @@ router.get("/supplier", supplierControllers.getSupplier);
 router.get("/supplier/:supplierID", supplierControllers.getSupplierDetails);
 router.post("/add-supplier", supplierControllers.postAddSupplier);
 router.put("/supplier/:supplierID", supplierControllers.updateSupplier);
+router.delete("/supplier/:supplierID", supplierControllers.deleteSupplier);
+
 
 module.exports = router;
